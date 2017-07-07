a3d5a9fce2df861d853ea0ab121cbe5f34e082fa
Sam Macbeth <sam@cliqz.com>
2017-07-07 16:23:57 +0200

    Make search work on other platforms

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { AppRegistry, StyleSheet, View } from 'react-native';
+import Search from './Search';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+const styles = function () {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 20,
+      backgroundColor: '#FFFFFF'
+    },
+  });
+};
 
-export default class CliqzS extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+// wrapper for a component to add top padding on iOS
+function AppContainer(App) {
+  return () => (
+    <View style={styles().container}>
+      <App />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('CliqzS', () => CliqzS);
+AppRegistry.registerComponent('CliqzS', () => AppContainer(Search));
