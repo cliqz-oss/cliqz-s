aec059ff6226a8d54d8ef3bc7d8a294ed7c86cbe
Krzysztof Jan Modras <chrmod@chrmod.net>
2018-02-24 20:47:42 +0100

    Setting up react-navigation and some project structure

---
+++ app/config/router.js
@@ -0,0 +1,12 @@
+import { DrawerNavigator } from 'react-navigation';
+
+import Home from '../screens/Home';
+import Drawer from '../components/Drawer';
+
+export default DrawerNavigator({
+  Home: {
+    screen: Home,
+  },
+}, {
+  contentComponent: Drawer
+});
