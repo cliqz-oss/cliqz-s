3d6b9816e2e4f562ec24628d0f94e2d5d2ff2fc6
Krzysztof Jan Modras <chrmod@chrmod.net>
2018-02-26 15:00:25 +0100

    Trying out different setup

--- e2e/init.js
+++ e2e/init.js
@@ -1,8 +1,8 @@
 const detox = require('detox');
 const config = require('../package.json').detox;
 
-// Set the default test timeout of 120s
-jest.setTimeout(120000 * 5);
+// Set the default test timeout of 240s
+jest.setTimeout(120000 * 2);
 
 beforeAll(async () => {
   await detox.init(config);
